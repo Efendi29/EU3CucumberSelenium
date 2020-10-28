d883e0f9b4abadd8c1d54ac6ed49a0c39bad6a05
Efendi29 <azerefem@gmail.com>
2020-10-28 20:53:14 +0100

    implicit wait  added to Hooks

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,5 +1,6 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -7,10 +8,14 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.util.concurrent.TimeUnit;
+
 public class Hooks {
     @Before
     public void setUp(){
         System.out.println("\tthis is coming from BEFORE");
+        Driver.get().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //Driver.get().manage().window().maximize();
     }
 
     @After
